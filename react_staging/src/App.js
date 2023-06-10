4b442931519b54a1572ad0f03d3ca2c7e0a344a9
yanqi17118357 <3224848830@qq.com>
2023-06-11 01:12:56 +0800

    TodoList案例勾选item框更改状态

--- react_staging/src/App.js
+++ react_staging/src/App.js
@@ -1,36 +1,51 @@
-import React, {Component} from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import List from './components/List';
-import './App.css';
+import React, {Component} from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import List from './components/List'
+import './App.css'
 export default class App extends Component {
+	state = {
+		todos: [
+			{id: '001', name: '吃饭', done: true},
+			{id: '002', name: '睡觉', done: true},
+			{id: '003', name: '打代码', done: false},
+		],
+	}
 
-	state = {todos:[
-		{id:'001', name:'吃饭', done: true},
-		{id:'002', name:'睡觉', done: true},
-		{id:'003', name:'打代码', done: false}
-	]}
-
-	addTodo = (todoObj) => {
+	addTodo = todoObj => {
 		// 获取当前状态
-		const {todos} = this.state;
+		const {todos} = this.state
 		// 创建新的todos
-		const newTodos = [todoObj, ...todos];
+		const newTodos = [todoObj, ...todos]
 		// 更新状态
-		this.setState({todos:newTodos});
+		this.setState({todos: newTodos})
+	}
+
+	updateTodo = (id, done) => {
+		// 获取当前状态
+		const {todos} = this.state
+		// 更改todos
+		const newTodos = todos.map((todoObj) => {
+			if(todoObj.id === id) {
+				return {...todoObj, done: done}
+			}
+			return todoObj
+		})
+
+		this.setState({todos: newTodos})
 	}
 
 	render() {
-		const { todos } = this.state;
+		const {todos} = this.state
 		return (
 			<div className='todo-container'>
 				<div className='todo-wrap'>
 					<Header addTodo={this.addTodo} />
 					{/*注意：传递参数的属性名称不能是关键字，比如delete*/}
-					<List todos={todos}/>
+					<List todos={todos} updateTodo={this.updateTodo} />
 					<Footer />
 				</div>
 			</div>
-		);
+		)
 	}
 }
--- react_staging/src/components/Item/index.jsx
+++ react_staging/src/components/Item/index.jsx
@@ -4,22 +4,29 @@ export default class Item extends Component {
 
 	state = {mouse:false}
 
-
+	// 标识鼠标移入、移出
 	handleMouse = (flag) => {
 		return () => {
 			this.setState({mouse:flag})
 		}
 	}
 
+	// 勾选、取消勾选某一个todo的回调
+	handleCheck = (id) => {
+			return (event) => {
+					this.props.updateTodo(id, event.target.checked)
+			}
+	}
+
 	render() {
-		const {name, done} = this.props
+		const {id, name, done} = this.props
 		const {mouse} = this.state
 
 		return (
 			<div>
 				<li style={{backgroundColor: mouse ? '#ddd' : 'white'}} onMouseLeave={this.handleMouse(false)} onMouseEnter={this.handleMouse(true)}>
 					<label>
-						<input type='checkbox' defaultChecked={done}/>
+						<input type='checkbox' defaultChecked={done} onChange={this.handleCheck(id)}/>
 						<span>{name}</span>
 					</label>
 					<button className='btn btn-danger' style={{display: mouse? 'block' : 'none'}}>删除</button>
