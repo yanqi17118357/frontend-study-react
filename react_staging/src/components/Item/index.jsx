addc812b501d2f80ca74e05518e4e6cf2c7ba82a
yanqi17118357 <3224848830@qq.com>
2023-06-10 22:00:23 +0800

    TodoList案例鼠标移入移出效果

--- react_staging/src/components/Item/index.jsx
+++ react_staging/src/components/Item/index.jsx
@@ -1,18 +1,28 @@
 import React, {Component} from 'react';
 import './index.css'
 export default class Item extends Component {
-	render() {
 
+	state = {mouse:false}
+
+
+	handleMouse = (flag) => {
+		return () => {
+			this.setState({mouse:flag})
+		}
+	}
+
+	render() {
 		const {name, done} = this.props
+		const {mouse} = this.state
 
 		return (
 			<div>
-				<li >
+				<li style={{backgroundColor: mouse ? '#ddd' : 'white'}} onMouseLeave={this.handleMouse(false)} onMouseEnter={this.handleMouse(true)}>
 					<label>
 						<input type='checkbox' defaultChecked={done}/>
 						<span>{name}</span>
 					</label>
-					<button className='btn btn-danger' style={{display: 'none'}}>删除</button>
+					<button className='btn btn-danger' style={{display: mouse? 'block' : 'none'}}>删除</button>
 				</li>
 			</div>
 		);
